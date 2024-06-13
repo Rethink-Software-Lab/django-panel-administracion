bf6209a77915f4626c85b48e7f09ebe5a793ac9a
Teby00 <tokkenpage@gmail.com>
2024-06-13 19:51:14 -0400

    fixed Allowed Hosts

--- project_inventario/settings.py
+++ project_inventario/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://panel-administracion.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
