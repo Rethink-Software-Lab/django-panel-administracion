3481b4ac7b072ffade6c1ae5956a1d16e8d403ad
Teby00 <tokkenpage@gmail.com>
2024-06-16 18:43:35 -0400

    asd

--- project_inventario/settings.py
+++ project_inventario/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles_build', 'static')
 AUTH_USER_MODEL = 'inventario.User'
 
 # Default primary key field type
