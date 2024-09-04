ca4544ced39843a58c1216ad668f62ccbb2820de
Teby00 <tokkenpage@gmail.com>
2024-09-04 01:23:07 -0400

    .

--- inventario_v2/controllers/usuarios.py
+++ inventario_v2/controllers/usuarios.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from ninja.errors import HttpError
 from inventario.models import AreaVenta, User
 from inventario_v2.custom_permissions import isAdmin
@@ -14,10 +15,12 @@ class UsuariosController:
 
     @route.post("")
     def addUsuario(self, data: UsuariosAuthSchema):
-        dataDict = data.model_dump() 
-        
+        dataDict = data.model_dump()
+
         try:
-            user = User.objects.create(username=dataDict["username"], rol=dataDict["rol"])
+            user = User.objects.create(
+                username=dataDict["username"], rol=dataDict["rol"]
+            )
 
             if dataDict["rol"] == "ADMIN":
                 user.is_staff = True
@@ -28,13 +31,16 @@ class UsuariosController:
 
             user.set_password(dataDict["password"])
             user.save()
-            return {"success": True}
-        except:
-            raise HttpError(500, "Error inesperado")
-        
+            return "Usuario creado con éxito."
+        except Exception as e:
+            if isinstance(e, IntegrityError):
+                raise HttpError(400, "El usuario ya existe.")
+            else:
+                raise HttpError(500, "Error inesperado.")
+
     @route.put("{id}/")
     def updateUsuario(self, id: int, data: UsuariosAuthSchema):
-        dataDict = data.model_dump() 
+        dataDict = data.model_dump()
         print(dataDict)
         try:
             user = get_object_or_404(User, pk=id)
@@ -45,12 +51,11 @@ class UsuariosController:
             if not dataDict["area_venta"]:
                 user.area_venta = None
             else:
-                print('khghg')
                 area = get_object_or_404(AreaVenta, pk=dataDict["area_venta"])
                 user.area_venta = area
             user.save()
             return {"success": True}
-        
+
         except:
             raise HttpError(500, "Error inesperado")
 
