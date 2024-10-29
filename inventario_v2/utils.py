ac7e08f072000db2596d35a5cab49597f1f7fe29
Teby00 <tokkenpage@gmail.com>
2024-10-29 12:20:56 -0400

    🕸️ extraer para mejor visibilidad

--- inventario_v2/utils.py
+++ inventario_v2/utils.py
@@ -1,3 +1,8 @@
+import calendar
+from datetime import datetime, timedelta
+from typing import Counter
+
+
 days_names = {
     0: "Lun",
     1: "Mar",
@@ -31,3 +36,35 @@ month_names = {
 
 def get_month_name(month_number):
     return month_names.get(month_number, "Mes no válido")
+
+
+def calcular_dias_laborables(desde, hasta):
+    delta = timedelta(days=1)
+    dias_laborables = 0
+    while desde <= hasta:
+        if desde.weekday() != 6:
+            dias_laborables += 1
+        desde += delta
+    return dias_laborables
+
+
+def obtener_inicio_fin_mes(anio, mes):
+    inicio_mes = datetime(anio, mes, 1)
+
+    ultimo_dia = calendar.monthrange(anio, mes)[1]
+    fin_mes = datetime(anio, mes, ultimo_dia)
+
+    return inicio_mes, fin_mes
+
+
+def obtener_ultimo_dia_mes(fecha):
+    _, ultimo_dia = calendar.monthrange(fecha.year, fecha.month)
+    return ultimo_dia
+
+
+def obtener_dias_semana_rango(desde, hasta):
+    dias_semana = Counter()
+    while desde <= hasta:
+        dias_semana[desde.weekday()] += 1
+        desde += timedelta(days=1)
+    return dias_semana
