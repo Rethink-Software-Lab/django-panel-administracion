e08c4ff0c5e7be50f30fd8576fbdebf6f70564fd
Esteban <estebarmartin@gmail.com>
2025-08-09 00:05:10 -0400

    agregando nueva propiedad al modelo area venta y eliminando controlados area venta

--- inventario/models.py
+++ inventario/models.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import (
 class AreaVenta(models.Model):
     nombre = models.CharField(max_length=50, blank=False, null=False)
     color = models.CharField(max_length=10)
+    isMesa = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
