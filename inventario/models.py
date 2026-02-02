4fd8ad36f5d265b42dccf3d11432077dc06bed6d
Esteban Daniel Rodríguez Martín <71105277+teby00@users.noreply.github.com>
2026-02-01 19:50:00 -0500

    agregando campo eliminado a Cuentas para gestionar registros eliminados

--- inventario/models.py
+++ inventario/models.py
@@ -46,6 +46,7 @@ class Cuentas(models.Model):
         max_length=50, choices=BancoChoices.choices, blank=True, null=True
     )
     active = models.BooleanField(default=True, null=False, blank=False)
+    eliminado = models.BooleanField(default=False, null=False, blank=False)
 
     def __str__(self):
         return self.nombre
